5437d36d4d5127de01dcba36a24e20e624d1659d
C.Y. Tseng <diella.tseng@gmail.com>
2021-08-18 17:26:50 +0200

    cut index.js into 3 functions; change theme to camera

--- js/index.js
+++ js/index.js
@@ -1,33 +1,43 @@
-const api_url = 'http://localhost:3000/api/teddies/';
+const api_url = 'http://localhost:3000/api/cameras/';
+const myProducts = {
+    productList: [],
+};
+let myProductsLength = 0;
 
-fetch (api_url)
-    .then(res => {
-        if(res.ok) {
-            return res.json()
-        } else {
-            console.log('Error')
-        }
-    })
-    //function to create product cards//
-    .then(productData => {
-        //get number of products//
-        let dataLength = productData.length;
-        //for each product data, create one card//
-        for (let i = 0; i < dataLength; i++) {
-            const products = document.getElementById('products');
-            const productImage = productData[i].imageUrl;
-            const productId = productData[i]._id;
-            const productName = productData[i].name;
-            const productPrice = '€ ' + (Number(productData[i].price/100).toFixed(2));
-            const product   = '<div class="col-6"><div class="card border-light"><img class="card-img-top" src="' 
-                            + productImage 
-                            + '" alt=""><div class="card-body px-0"><a class="text-decoration-none text-dark stretched-link" href="/product/' 
-                            + productId 
-                            + '.html"><h3 id="productTitle" class="card-title h6 mb-1">'
-                            + productName 
-                            + '</h3></a><p id="productPrice" class="card-text">'
-                            + productPrice 
-                            + '</p></div></div></div>';
-            products.innerHTML += product;
-        }
-    })
+//Get all product data from server, put string into myProducts//
+const getProductData = async () => {
+    const response = await fetch(api_url);
+    const jsonProductData = await response.json();
+    myProducts.productList = jsonProductData;
+}
+
+//Get length of products list (number of products)//
+const getNumberOfProducts = async () => {
+    await getProductData();
+    myProductsLength = myProducts.productList.length;
+}
+
+// for each product in myProducts, create one card//
+const createCards = async() => {
+    await getNumberOfProducts();
+    let myProductData = myProducts.productList;
+    for (let i = 0; i < myProductsLength; i++) {
+        const divProducts = document.getElementById('divProducts');
+        const productImage = myProductData[i].imageUrl;
+        const productId = myProductData[i]._id;
+        const productName = myProductData[i].name;
+        const productPrice = '€ ' + (Number(myProductData[i].price/100).toFixed(2));
+        const product   = '<li class="col-6"><div class="card border-light"><img class="card-img-top" src="' 
+                        + productImage 
+                        + '" alt=""><div class="card-body px-0"><a class="text-decoration-none text-dark stretched-link" href="product.html?productId=' 
+                        + productId 
+                        + '"><h3 id="productTitle" class="card-title h6 mb-1">'
+                        + productName 
+                        + '</h3></a><p id="productPrice" class="card-text">'
+                        + productPrice 
+                        + '</p></div></div></li>';
+        divProducts.innerHTML += product;
+    }
+}
+
+createCards();
