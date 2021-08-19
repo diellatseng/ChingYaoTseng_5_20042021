b79fca06e404b34023b18adedf16e0da168c9246
C.Y. Tseng <diella.tseng@gmail.com>
2021-08-19 14:51:53 +0200

    add product demo page

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,43 +1,30 @@
 const apiUrl = 'http://localhost:3000/api/cameras/';
-const myProducts = { 
-    productList: [],
-};
-let myProductsLength = 0;
+let myProducts = [];
 
-//Get all product data from server, put string into myProducts//
+//Get product data from server, store string into myProducts//
 const getProductData = async () => {
-    const response = await fetch(apiUrl);
-    const jsonProductData = await response.json();
-    myProducts.productList = jsonProductData;
+    let response = await fetch(apiUrl);
+    let jsonData = await response.json();
+    myProducts = jsonData;
 }
 
-//Get length of products list (number of products)//
-const getNumberOfProducts = async () => {
+// For each product in myProducts, create one card//
+const createProductCards = async() => {
     await getProductData();
-    myProductsLength = myProducts.productList.length;
-}
-
-// for each product in myProducts, create one card//
-const createCards = async() => {
-    await getNumberOfProducts();
-    let myProductData = myProducts.productList;
-    for (let i = 0; i < myProductsLength; i++) {
-        const divProducts = document.getElementById('divProducts');
-        const productImage = myProductData[i].imageUrl;
-        const productId = myProductData[i]._id;
-        const productName = myProductData[i].name;
-        const productPrice = '€ ' + (Number(myProductData[i].price/100).toFixed(2));
-        const product   = '<li class="col-6"><div class="card border-light"><img class="card-img-top" src="' 
-                        + productImage 
-                        + '" alt=""><div class="card-body px-0"><a class="text-decoration-none text-dark stretched-link" href="product.html?id=' 
-                        + productId 
-                        + '"><h3 id="productTitle" class="card-title h6 mb-1">'
-                        + productName 
-                        + '</h3></a><p id="productPrice" class="card-text">'
-                        + productPrice 
-                        + '</p></div></div></li>';
-        divProducts.innerHTML += product;
+    for (product of myProducts) {
+        const divProducts   = document.getElementById('divProducts');
+        const productPrice  = '€ ' + (Number(product.price/100).toFixed(2));
+        const productHtml   = '<li class="col-6"><div class="card border-light"><img class="card-img-top" src="' 
+                            + product.imageUrl
+                            + '" alt=""><div class="card-body px-0"><a class="text-decoration-none text-dark stretched-link" href="/frontend/product.html?id=' 
+                            + product._id
+                            + '"><h3 id="productTitle" class="card-title h6 mb-1">'
+                            + product.name
+                            + '</h3></a><p id="productPrice" class="card-text">'
+                            + productPrice 
+                            + '</p></div></div></li>';
+        divProducts.innerHTML += productHtml;
     }
 }
 
-createCards();
+createProductCards();
