6171e906bd99354a5fbd23e675a304a6980b75e7
C.Y. Tseng <diella.tseng@gmail.com>
2021-08-20 10:43:27 +0200

    create cart.html

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -2,15 +2,15 @@ const apiUrl = 'http://localhost:3000/api/cameras/';
 let myProducts = [];
 
 //Get product data from server, store string into myProducts//
-const getProductData = async () => {
+const getData = async () => {
     let response = await fetch(apiUrl);
     let jsonData = await response.json();
     myProducts = jsonData;
 }
 
 // For each product in myProducts, create one card//
-const createProductCards = async() => {
-    await getProductData();
+const displayProducts = async() => {
+    await getData();
     for (product of myProducts) {
         const divProducts   = document.getElementById('divProducts');
         const productPrice  = '€ ' + (Number(product.price/100).toFixed(2));
@@ -27,4 +27,4 @@ const createProductCards = async() => {
     }
 }
 
-createProductCards();
+displayProducts();
