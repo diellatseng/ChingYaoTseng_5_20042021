443c4cd47d653bc4e2fbc864b6c6d93ad0f774d2
C.Y. Tseng <diella.tseng@gmail.com>
2021-08-25 19:46:25 +0200

    starting submit button

--- frontend/js/components/confirmOrderBtn.js
+++ frontend/js/components/confirmOrderBtn.js
@@ -1,3 +1,11 @@
 // - Confirm order button
 // - Remove SessionStorage
+const myForm = document.getElementById('myForm');
 
+myForm.addEventListener('submit', function (e) {
+    e.preventDefault;
+    const data = Array.from(document.querySelectorAll('#myForm input'));
+    
+    console.log(data);
+
+})
