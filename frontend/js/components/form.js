53248cc24c18d5c4b2e712c23920772c65f17745
C.Y. Tseng <diella.tseng@gmail.com>
2021-08-25 18:41:26 +0200

    add postal code api to form

--- frontend/js/components/form.js
+++ frontend/js/components/form.js
@@ -1,17 +1,18 @@
-let zipInput = document.getElementById('zip');
+let postalCode = document.getElementById('postalCode');
 const city = document.getElementById('city');
 
-//Search for a city with its postal code.
-//The variable postalcode allows you to retrieve the list of communes associated with a postalcode.
-zipInput.onchange = (e) => {
-    let zip = e.target.value;
+//Convert postalcode to city name:
+//retrieve the list of communes associated with a postalcode, and then display it to "commune".
+postalCode.onchange = (e) => {
+    let postalCode = e.target.value;
     try {
         const getData = async () => {
-            let response = await fetch(`https://geo.api.gouv.fr/communes?codePostal=${zip}`);
+            let response = await fetch(`https://geo.api.gouv.fr/communes?codePostal=${postalCode}`);
             let jsonData = await response.json();
             if(jsonData != '') {
                 document.getElementById('city').value = jsonData[0].nom;
             } else {
+                //If postalcode entered is not valid, return alert message to user
                 window.alert("Veuillez entrer le code postal correct")
             }
             console.log(jsonData);
